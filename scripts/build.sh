4ba92c877c5ee913df49d93188867173dc7f8d63
tripleawwy <hansi_mdb@hotmail.de>
2024-11-17 20:55:04 +0100

    feat: add possibility to create multiplatform images

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-for PLATFORM in "linux/amd64"; do
-    GOOS=${PLATFORM%/*}
-    GOARCH=${PLATFORM#*/}
+# get PLATFORMS from the first argument with a default value
+PLATFORM=${1:-"linux/amd64"}
+GOOS=${PLATFORM%/*}
+GOARCH=${PLATFORM#*/}
 
-    BIN_FILE_NAME="bin/ol_discord_bot-${GOOS}-${GOARCH}"
+BIN_FILE_NAME="bin/ol_discord_bot"
 
-    echo Building ${GOOS}-${GOARCH} version ${OLS_VERSION}...
+echo "Building ${GOOS}-${GOARCH}..."
 
-    GOOS="${GOOS}" GOARCH="${GOARCH}" \
-    go build -o "${BIN_FILE_NAME}" \
-    || FAILURES="${FAILURES} ${GOOS}-${GOARCH}"
-done
+GOOS="${GOOS}" GOARCH="${GOARCH}" \
+go build -ldflags="-w -s" -o "${BIN_FILE_NAME}" \
+|| FAILURES="${FAILURES} ${GOOS}-${GOARCH}"
 
 if [ -n "${FAILURES}" ]; then
-    echo Build Failures:
-    for FAILURE in $FAILURES; do
-        echo $FAILURE
-    done
-    exit 1
+  echo Build Failures:
+  for FAILURE in $FAILURES; do
+    echo "${FAILURE}"
+  done
+  exit 1
 fi
